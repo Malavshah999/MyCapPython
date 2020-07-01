3d5ed90dae867ea85bcf3e47c766b75e26351efe
Malavshah999 <56575479+Malavshah999@users.noreply.github.com>
2020-07-01 13:00:35 +0530

    Create PositiveIntegers.py

---
+++ PositiveIntegers.py
@@ -0,0 +1,15 @@
+#Checking Positive Integers in given series 
+
+list1 = [10, -21, 4, -45, 66 ]
+for num in list1: 
+ 
+    if num >= 0: 
+       print("List1 :[" , num , "]") 
+       
+       
+
+list2 = [1, -11, 40, -4, 8 ]
+for num in list2: 
+ 
+    if num >= 0: 
+       print("List2 :[" , num , "]" ) 
